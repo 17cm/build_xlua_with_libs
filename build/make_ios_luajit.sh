30e2ea5972354d757c1db40c3dcaa90f321f0ca7
Timmy <chentm@gmail.com>
2023-06-01 14:26:14 +0800

    update ndk

--- build/make_ios_luajit.sh
+++ build/make_ios_luajit.sh
@@ -49,7 +49,7 @@ fi
 cd ../..
 
 mkdir -p build_lj_ios && cd build_lj_ios
-cmake -DUSING_LUAJIT=ON  -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DPLATFORM=OS64  -GXcode ../
+cmake -DPBC=ON -DUSING_LUAJIT=ON  -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DPLATFORM=OS64  -GXcode ../
 cd ..
 cmake --build build_lj_ios --config Release
 
